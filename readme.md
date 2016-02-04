8901ebd0c78c076e7df3fdf1facbf4e0d22ba5a1
James McIntyre <james.checks.his.email@gmail.com>
2016-02-04 09:26:18 -0800

    added readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Trade Space
+
+An app to trade goods.
+
+View a [deployed version here](https://radiant-temple-68975.herokuapp.com/)!
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Glass Cats" });
+  res.render('login', { title: "Glass Cats" });
 });
 
 
